e1dfe09f745dbe0420dc294d508b5c31e6dcc6ff
Aleksei Starovoitov <morbiuzx@gmail.com>
2019-08-06 08:04:27 +0300

    PyNEng/05 - task_5_2 Done

--- Python/PyNEng/05_basic_scripts/task_5_2.py
+++ Python/PyNEng/05_basic_scripts/task_5_2.py
@@ -19,3 +19,20 @@ Mask:
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+network, mask = input('Enter network and mask like 10.1.1.0/24: ').split('/')
+mask_bin = (int(mask) * '1') + ((32 - int(mask)) * '0')
+mask_dec = [int(mask_bin[:8], 2), int(mask_bin[8:16], 2), int(mask_bin[16:24], 2), int(mask_bin[24:32], 2)]
+network = network.split('.')
+
+template = '''
+Network:
+{0:<8} {1:<8} {2:<8} {3:<8}
+{0:08b} {1:08b} {2:08b} {3:08b}
+
+Mask:
+{8:}
+{4:<8} {5:<8} {6:<8} {7:<8}
+{4:08b} {5:08b} {6:08b} {7:08b}
+'''
+
+print(template.format(int(network[0]), int(network[1]), int(network[2]), int(network[3]), mask_dec[0], mask_dec[1], mask_dec[2], mask_dec[3], '/' + mask))
