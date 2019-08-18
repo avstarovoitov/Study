63f6946ac6a587dbfd8fd1123de2271cb76ff986
Алексей Старовойтов <avstarovoitov@yamoney.ru>
2019-08-18 16:34:09 +0300

    PyNEng/07 - task_7_3b Done

--- Python/PyNEng/07_files/task_7_3b.py
+++ Python/PyNEng/07_files/task_7_3b.py
@@ -11,3 +11,28 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+
+###
+
+with open('CAM_table.txt') as cam:
+    lines = []
+    for line in cam:
+        try:
+            if line[1].isdigit():
+                lines.append(line)
+        except IndexError:
+            pass
+vlans = sorted({int(vlan[1:5]) for vlan in lines})
+input_vlan = int(input('Enter vlan number from ' + str(vlans) + ' : '))
+output_dict = dict.fromkeys(vlans)
+
+for line in lines:
+    if not output_dict[int(line[1:5])]:
+        output_dict[int(line[1:5])] = [line.rstrip()]
+    else:
+        temp_value = output_dict[int(line[1:5])]
+        temp_value.append(line)
+        output_dict[int(line[1:5])] = temp_value
+
+for line in output_dict[input_vlan]:
+    print(' {:<8} {:<16} {}'.format(line.split()[0], line.split()[1], line.split()[3]))
